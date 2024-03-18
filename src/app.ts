59ad586a11bb2fcd71892ad1180fa773be4b6cb8
romanshalaev <mc.shel24@gmail.com>
2024-03-18 19:35:06 +0200

    feat: added types DeepReadonly, DeepRequireReadonly, UpperCaseKeys, ObjectToPropertyDescriptor

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,19 @@ function showHello(divName: string, name: string) {
     const elt = document.getElementById(divName);
     elt!.innerText = `Hello from ${name}`;
 }
+
+type DeepReadonly<T> = {
+    readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P];
+};
+
+type DeepRequireReadonly<T> = {
+    readonly [P in keyof T]-?: T[P] extends object ? DeepRequireReadonly<T[P]> : T[P];
+};
+
+type UpperCaseKeys<T> = {
+    [K in keyof T as Uppercase<string & K>]: T[K];
+};
+
+type ObjectToPropertyDescriptor<T> = {
+    [K in keyof T]: PropertyDescriptor;
+};
